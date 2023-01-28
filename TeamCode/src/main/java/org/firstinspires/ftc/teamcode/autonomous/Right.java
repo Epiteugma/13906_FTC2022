fae5bce9d9a5069d1c0a31b70b4eb16ca7d3a956
z3db0y <us3r0002@gmail.com>
2023-01-28 17:51:46 +0200

    undo auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Right.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Right.java
@@ -18,43 +18,11 @@ public class Right extends AutonomousOpMode {
     }
 
     public void center() {
-        for(int i = 0; i < 2; i++) {
-            driveTrain.turn(180, 1, imu, false);
-            slideMotors.runToPosition(TickUtils.cmToTicks(15 - (i * 5), 28 * 18, slideGearRadius), 1);
-            driveTrain.driveCM(51 +i*2, 0.9, DriveTrain.Direction.FORWARD);
-            closeClaw();
-            slideMotors.runToPosition(TickUtils.cmToTicks(77 + i*5, 28 * 18, slideGearRadius), 1);
-//            slideMotors.runToPositionAsync(TickUtils.cmToTicks(50 + i * 5, 28 * 18, slideGearRadius), 1);
-            driveTrain.driveCM(39 + i*2, 0.9, DriveTrain.Direction.BACKWARD);
-            driveTrain.turn(-45, 1, imu, false);
-            driveTrain.driveCM(15, 1, DriveTrain.Direction.FORWARD);
-//            extension.runToPosition(TickUtils.cmToTicks(-22, 288, extensionGearRadius), 1);
-            slideMotors.runToPosition(0, 1, false);
-            openClaw();
-//            extension.runToPosition(0, 1, false);
-            driveTrain.driveCM(15, 1, DriveTrain.Direction.BACKWARD);
-        }
-        driveTrain.driveCM(3, 1, DriveTrain.Direction.BACKWARD);
+
     }
 
     public void right() {
-//        driveTrain.driveCM(55, 1, DriveTrain.Direction.BACKWARD);
-//        releaseRotatingBaseServo(true);
-//        driveTrain.strafeCM(55, 1, DriveTrain.Direction.RIGHT);
-//        driveTrain.turn(-90, 1, imu, false);
-//        driveTrain.driveCM(5, 1, DriveTrain.Direction.FORWARD);
-//        slideMotors.runToPosition(TickUtils.cmToTicks(-20, 28 * 18, slideGearRadius), 1);
-//        rotatingBaseHelper.runTo(37, 0.6);
-//        extension.runToPosition(-TickUtils.cmToTicks(40, 288, extensionGearRadius), 1);
-//        closeClaw();
-//        slideMotors.runToPosition(TickUtils.cmToTicks(25, 28 * 18, slideGearRadius), 1);
-//        rotatingBaseHelper.runTo(-58, 0.6);
-//        extension.runToPosition(TickUtils.cmToTicks(25, 288, extensionGearRadius), 1);
-//        slideMotors.runToPosition(-TickUtils.cmToTicks(30, 28 * 18, slideGearRadius), 1);
-//        openClaw();
-//        extension.runToPosition(TickUtils.cmToTicks(4, 288, extensionGearRadius), 1);
-//        driveTrain.strafeCM(10, 1, DriveTrain.Direction.RIGHT);
-//        driveTrain.turn(-90, 1, imu, false);
+
     }
 
     @Override
