3308a3b96e79e7ea97a5867f94f21d4313696163
z3db0y <us3r0002@gmail.com>
2023-03-05 19:32:51 +0200

    deleted old code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/REVVED_UP_JR.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Enums;
+import org.firstinspires.ftc.teamcode.Flags;
+
+@TeleOp(name="HDrive 2", group="FTC22")
+@Flags(robotType = Enums.RobotType.REVVED_UP_JR)
+//@Disabled
+public class REVVED_UP_JR extends TeleOpMode {}
